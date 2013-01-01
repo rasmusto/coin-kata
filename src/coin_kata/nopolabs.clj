3f0b7dec012cf775ad53199567548bb458afe463
Dan Revel <drevel@gilt.com>
2012-12-31 19:48:06 -0800

    tidied up

--- src/coin_kata/nopolabs.clj
+++ src/coin_kata/nopolabs.clj
@@ -1,217 +1,115 @@
-(ns coin-kata.nopolabs
+(ns coin-kata.np-lcm-search
   (:require [clojure.math.numeric-tower :as math]))
-  
-; in repl: (use :reload 'coin-kata.nopolabs)
 
-(def us-coins [1 5 10 25])
+;; in repl: (use :reload 'coin-kata.np-lcm-search)
+
 (def x-coins [1 10 20 25])
+(def us-coins [1 5 10 25])
 
+;; change is a map from denoms to quantities
 (defn init-change [denoms] (zipmap denoms (repeat 0)))
-(defn coin-count [change] (apply + (vals change)))
-(defn expand-denoms [denoms] (reverse (map reverse (drop 1 (reductions conj [] denoms)))))
-(defn least-coins [solutions] (first (sort-by coin-count solutions)))
-(defn use-denom [denom amount] [(quot amount denom) (rem amount denom)])
-(defn apply-denom
-  [denom amount change]
-  (let [[n-denom next-amount] (use-denom denom amount)
-         next-change (assoc change denom n-denom)]
-    [next-amount next-change]))
 
-(defn chg
-  [make-change denoms amounts]
-  (let [amts (if (coll? amounts) amounts [amounts])
-        mk-chg (partial make-change denoms)]
-    (map mk-chg amts)))
-    
-(defn us-chg [make-change amounts] (chg make-change us-coins amounts))
+(defn coin-count [change] (reduce + (vals change)))
 
-(defn x-chg [make-change amounts] (chg make-change x-coins amounts))
+(defn add-change [x y] (into {} (map (fn [k] [k (+ (get x k) (get y k 0))]) (keys x))))
 
-(defn sgc
-  "simple greedy change"
-  ([denoms amount]
-    (let [change (init-change denoms)]
-      (sgc (sort > denoms) amount change)))
-  ([[denom & next-denoms] amount change]
-    (cond
-      (= 0 amount) change
-      (nil? denom) nil
-      :else
-        (let [[next-amount next-change] (apply-denom denom amount change)]
-          (sgc next-denoms next-amount next-change)))))
+(defn add-coins [change denom n] (assoc change denom (+ (get change denom) n)))
+  
+(defn add-coin [change denom] (add-coins change denom 1))
+  
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Depth first search
 
-(defn sgcr
-  "simple greedy change recursive"
-  ([denoms amount]
-    (let [change (init-change denoms)]
-      (loop [[d & ds] (sort > denoms)
-             a amount 
-             c change]
-        (cond
-          (= 0 a) c
-          (nil? d) nil
-          :else
-            (let [[next-a next-c] (apply-denom d a c)]
-              (recur ds next-a next-c)))))))
-
-(defn mc2
-  ([denoms amount]
-    (let [change (init-change denoms)
-          solutions (for [ds (expand-denoms denoms)] (mc2 ds amount change))]
-      (least-coins solutions)))
-  ([[denom & next-denoms] amount change]
-    (cond
-      (= 0 amount) change
-      (nil? denom) nil
-      :else
-        (let [[next-amount next-change] (apply-denom denom amount change)]
-          (mc2 next-denoms next-amount next-change)))))
-
-(defn mc3
-  ([denoms amount]
-    (let [change (init-change denoms)
-          solutions (map #(mc3 % amount change) (expand-denoms denoms))]
-      (least-coins solutions)))
-  ([[denom & next-denoms] amount change]
-    (cond
-      (= 0 amount) change
-      (nil? denom) nil
-      :else
-        (let [[next-amount next-change] (apply-denom denom amount change)]
-          (mc3 next-denoms next-amount next-change)))))
-
-(defn possible? [{ a :amt } amt] (<= a amt))
-
-(defn correct-change? [{ a :amt } amt] (= a amt))
-
-(defn greatest-possible
-  [x y]
-  (let [x-amt (:amt x)
-        y-amt (:amt y)]
-    (> x-amt y-amt)))
-
-(defn expand-possible
-  [denoms amount possible]
-  (apply sorted-set-by greatest-possible
-    (filter #(possible? % amount)
-      (for [{ a :amt n :n c :change} possible d denoms]
-        (let [new-a (+ d a)
-              new-n (inc n)
-              new-c (assoc c d (inc (get c d)))]
-          { :amt new-a :n new-n :change new-c })))))
-
-(defn init-possible [denoms] (list { :amt 0 :n 0 :change (init-change denoms) }))
-
-(defn mc5
-  ([denoms amount]
-    (mc5 denoms amount (init-possible denoms)))
-  ([denoms amount possible]
-    (if (= amount (:amt (first possible)))
-      (:change (first possible))
-      (mc5 denoms amount (expand-possible denoms amount possible)))))
-
-(defn mc5r
-  ([denoms amount]
-    (loop [d denoms a amount p (init-possible denoms)]
-      (if (= a (:amt (first p)))
-        (:change (first p))
-        (recur d a (expand-possible d a p))))))
-
-(defn -possible? [[a n c] amt] (<= a amt))
-
-(defn -correct-change? [a amt] (= a amt))
-
-(defn -greatest-possible [[x] [y]] (> x y))
-
-(defn -expand-possible
-  [denoms amount possible]
-  (apply sorted-set-by -greatest-possible
-    (filter #(-possible? % amount)
-      (for [[a n c] possible d denoms]
-        (let [new-a (+ d a)
-              new-n (inc n)
-              new-c (assoc c d (inc (get c d)))]
-          [new-a new-n new-c])))))
-
-(defn -init-possible [denoms] (list [0 0 (init-change denoms)]))
-
-(defn -mc5
-  ([denoms amount]
-    (-mc5 denoms amount (-init-possible denoms)))
-  ([denoms amount [[a n c] :as possible]]
-    (if (= amount a)
-      c
-      (let [p (seq (-expand-possible denoms amount possible))]
-        (-mc5 denoms amount p)))))
-
-(defn -mc5r
-  ([denoms amount]
-    (loop [p (-init-possible denoms)]
-      (if (= amount (first (first p)))
-        (nth (first p) 2)
-        (recur (-expand-possible denoms amount p))))))
-
-(defn greedy-changer
-  [denom]
-  (fn [limit]
-    (let [n (quot limit denom)
-          amount (* n denom)
-          remaining (rem limit denom)]
-      { :denom denom :count n :amount amount :remaining remaining })))
-          
-(defn mc
-  "returns a function that makes change using given denoms"
-  [denoms]
-  (let [sorted-denoms (sort > denoms)
-        changers (zipmap sorted-denoms (map greedy-changer sorted-denoms))]
-    (fn [amount]
-      (loop [a amount
-             [d & ds] sorted-denoms
-             change (init-change sorted-denoms)]
-        (if (= a 0) change
-          (let [ c ((get changers d) a)
-                next-a (:remaining c)
-                next-change (assoc change d (:count c))]
-            (recur next-a ds next-change)))))))
-
-(defn add-coin [d c] (assoc c d (inc (get c d))))
-
-(defn more-change
-  [denoms possible]
-  (for [[a n c] possible d denoms]
-    (let [new-a (+ d a)
-          new-n (inc n)
-          new-c (assoc c d (inc (get c d)))]
-      [new-a new-n new-c])))
-
-(defn amount= [[a n c] amt] (= a amt))
-
-(defn amount< [[a n c] amt] (< a amt))
-
-(defn possible-change
-  ([denoms] (possible-change denoms (list [0 0 (init-change denoms)])))
-  ([denoms possible]
-    (concat
-      possible
-      (lazy-seq
-        (let [p (more-change denoms possible)]
-          (possible-change denoms p))))))
-
-; (first (drop-while #(amount< % 50) (possible-change x-coins)))
-
-
-(defn least-common-multiple [coll] (reduce math/lcm 1 coll))
-
-(defn lcm-greedy
+(defn depth-first-change
   [denoms amount]
-  (loop [remaining-amount amount
-         remaining-denoms (sort > denoms)
-         change (init-change denoms)]
-    (if (= 0 remaining-amount) change
-      (let [lcm (least-common-multiple remaining-denoms)
-            d (first remaining-denoms)
-            n (* (/ lcm d) (quot remaining-amount lcm))
-            r (rem remaining-amount lcm)]
-        (recur r (rest remaining-denoms) (assoc change d n))))))
+  (letfn
+    [(best-change [left right]
+       (if (some nil? [left right])
+         (or left right)
+           (if (< (coin-count left) (coin-count right))
+             left right)))
 
+     (look-deeper [denoms amount change]
+       (let [d (first denoms)
+             left (search denoms (- amount d) (add-coin change d))
+             right (search (next denoms) amount change)]
+         (best-change left right)))
+
+     (search [denoms amount change]
+       (cond
+         (zero? amount) change
+         (< amount 0) nil
+         (empty? denoms) nil
+         :else (look-deeper denoms amount change)))]
+
+    (search (sort > denoms) amount (init-change denoms))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Breadth first search
+
+(defn breadth-first-change
+  [denoms amount]
+  (letfn
+    [(init-possible [denoms] (list [0 (init-change denoms)]))
+
+     (greatest-possible [[x-amt] [y-amt]] (> x-amt y-amt))
+
+     (possible? [[amt chg] max-amount] (<= amt max-amount))
+
+     (new-possible [[amt chg] d] [(+ d amt) (add-coin chg d)])
+
+     (next-possible [possible denoms amount]
+       (apply sorted-set-by greatest-possible
+         (filter #(possible? % amount)
+           (for [p possible d denoms]
+             (new-possible p d)))))]
+
+    (loop [possible (init-possible denoms)]
+      (let [first-possible (first possible)]
+        (if (= amount (first first-possible))
+          (second first-possible)
+          (recur (next-possible possible denoms amount)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; LCM optimization
+
+(defn lcm-opt
+  [chgfn]
+  (fn [denoms amount]
+    (let [ds (sort > denoms)
+          lcm (reduce math/lcm 1 ds)]
+      (if (< amount (* 2 lcm))
+        (chgfn ds amount)
+        (let [chg-amt (+ (rem amount lcm) lcm)
+              chg (chgfn ds chg-amt)
+              lcm-amt (- amount chg-amt)
+              d (first ds)
+              n (/ lcm-amt d)]
+          (add-coins chg d n))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; 
+
+(def df-opt (lcm-opt depth-first-change))
+(def bf-opt (lcm-opt breadth-first-change))
+
+(def df-x (partial depth-first-change x-coins))
+(def bf-x (partial breadth-first-change x-coins))
+(def df-opt-x (partial df-opt x-coins))
+(def bf-opt-x (partial bf-opt x-coins))
+
+(defn test-x [n]
+  (filter (complement nil?)
+    (for [i (range 1 (inc n))]
+      (let [nd (coin-count (df-x i))
+            nb (coin-count (bf-x i))
+            ndo (coin-count (df-opt-x i))
+            nbo (coin-count (bf-opt-x i))]
+        (if (= nd nb ndo nbo)
+          nil
+          i)))))
 
--- test/coin_kata/nopolabs_test.clj
+++
@@ -1,5 +0,0 @@
-(ns coin-kata.nopolabs-test
-  (:use .test
-        coin-kata.nopolabs))
-
-; in repl: (use :reload 'coin-kata.nopolabs-test)
